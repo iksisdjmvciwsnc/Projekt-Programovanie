1820ca2f5036e78fd08307bd5622d8797de687af
jan.dubrovcak@gmail.com <jan.dubrovcak@gmail.com>
2026-05-26 20:55:51 +0200

    Už naozaj opravené PHP súbory

--- db.php
+++ db.php
@@ -1,9 +1,7 @@
 <?php
-
 $conn = new mysqli("localhost", "root", "", "film_app");
 
 if ($conn->connect_error) {
     die("Chyba pripojenia: " . $conn->connect_error);
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+<!DOCTYPE html>
+<html lang="sk">
 <?php
 // Pripojíme sa k databáze
 include "db.php";
