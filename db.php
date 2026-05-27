091c775548868eafc6caa77d784f0f318a622914
jan.dubrovcak@gmail.com <jan.dubrovcak@gmail.com>
2026-05-27 08:02:38 +0200

    Oprava hesla v db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "film_app");
+$conn = new mysqli("localhost", "root", "root", "film_app");
 
 if ($conn->connect_error) {
     die("Chyba pripojenia: " . $conn->connect_error);
